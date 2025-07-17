30be71c871637ae6f0f38099260213d65ec40e24
Adrien Cadet <acadet@live.fr>
2025-07-17 11:28:37 +0200

    Add puzzles

---
+++ puzzles.js
@@ -0,0 +1,11 @@
+const puzzles = [
+    {
+        date: "2025-07-17",
+        words: [
+            ["Vert", "Orange", "Bleu", "Violet"],
+            ["Verre", "Tasse", "Gobelet", "Timbale"],
+            ["Vers", "Alexandrin", "Sonnet", "Rythme"],
+            ["Avocat", "Citron vert", "Ananas", "Coriandre"],
+        ]
+    }
+]; 
